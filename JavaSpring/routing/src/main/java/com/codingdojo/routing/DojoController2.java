8b15c1ca8ce7ccf797f31d25ba428e961f28572f
Shane Chintatkun <schintatkun@gmail.com>
2018-09-10 17:54:22 -0700

    added display Date

--- JavaSpring/routing/src/main/java/com/codingdojo/routing/DojoController2.java
+++ JavaSpring/routing/src/main/java/com/codingdojo/routing/DojoController2.java
@@ -16,12 +16,12 @@ public class DojoController2 {
 	@RequestMapping("/{track}")
     public String showLesson(@PathVariable("track") String track){
     	if (track.equals("dojo")){
-    		return "The dojo is awesome!";
+    		track = "The dojo is awesome!";
     	}else if (track.equals("burbank-dojo")) {
-    		return "Burbank Dojo is located in Southern California"; 
+    		track = "Burbank Dojo is located in Southern California"; 
     	}else if (track.equals("san-jose")) {
-    		return "SJ dojo is the headquearters";		
+    		track= "SJ dojo is the headquearters";		
     	}
-    	return "enter URL ";
+    	return track;
     }
 }
