ef97e1e546a2812e6c19a20f15920f3b5baeebaa
Shane Chintatkun <schintatkun@gmail.com>
2018-09-16 15:30:25 -0700

    added License assignment

--- JavaSpring/languages/src/main/java/com/codingdojo/languages/controllers/mainController.java
+++ JavaSpring/languages/src/main/java/com/codingdojo/languages/controllers/mainController.java
@@ -11,36 +11,37 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import com.codingdojo.languages.models.Languages;
 import com.codingdojo.languages.services.LanguageService;
 
 @Controller
 public class mainController {
 	private final LanguageService languageService;
+
 	public mainController(LanguageService languageService) {
 		this.languageService = languageService;
 	}
-	//redirect to /languages
+
+	// redirect to /languages
 	@RequestMapping("/")
 	public String redirectToIndex() {
 		return "redirect:/languages";
 	}
+
 	@RequestMapping("/languages")
 	public String index(Model model, @ModelAttribute("language") Languages language) {
 		List<Languages> langs = languageService.allLanguages();
 		model.addAttribute("languages", langs);
 		return "index.jsp";
 	}
-	
-	@RequestMapping(value="/create", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
 	public String create(@Valid @ModelAttribute("language") Languages language, BindingResult result) {
 		if (result.hasErrors()) {
-			return "index.jsp"; 
-			//must be rendering page, cannot be redirect otherwise errors would get thrown out
+			return "index.jsp";
+			// must be rendering page, cannot be redirect otherwise errors would get thrown out
 			// return "/languages"; does not work.
-		}else {
+		} else {
 			languageService.createLanguage(language);
 			return "redirect:/languages";
 		}
@@ -49,28 +50,30 @@ public class mainController {
 	@RequestMapping("/languages/{id}")
 	public String showLang(@PathVariable("id") Long id, Model model) {
 		Languages lang = languageService.findLang(id);
-		model.addAttribute("lang",lang);
+		model.addAttribute("lang", lang);
 		return "showLang.jsp";
 	}
+
 	// delete routing
 	@RequestMapping("/languages/delete/{id}")
-    public String delete(@PathVariable("id") Long id) {
+	public String delete(@PathVariable("id") Long id) {
 		languageService.deleteLanguage(id);
 		return "redirect:/languages";
 	}
-	
+
 	// edit routing
 	@RequestMapping("/languages/edit/{id}")
 	public String edit(@PathVariable("id") Long id, Model model) {
-		//edit a form
+		// edit a form
 		Languages lang = languageService.findLang(id);
-		model.addAttribute("lang",lang);
+		model.addAttribute("lang", lang);
 		return "edit.jsp";
 	}
+
 	@RequestMapping("/update/{id}")
-	public String update(@Valid @ModelAttribute("lang")Languages lang,BindingResult result) {
-		languageService.createLanguage(lang);	
+	public String update(@Valid @ModelAttribute("lang") Languages lang, BindingResult result) {
+		languageService.createLanguage(lang);
 		return "redirect:/languages";
+				
 	}
-	
 }
